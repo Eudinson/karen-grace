c4cfc168f4d3f137d7d97e1fd80b9811aba3c0e9
Eudinson <eudinson_uy29@yahoo.com>
2021-12-21 12:06:46 +0400

    saving project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import logo from './assets/instagram.png'
+
+const App = () => {
+  return(
+    <>
+      <p className='header-1'>Coming soon</p>
+      <img className='camera-icon' src={logo} alt="camera" />
+      <p className='header-2'>Karen Grace Photography</p>
+    </>
+  )
+}
+
+export default App;
